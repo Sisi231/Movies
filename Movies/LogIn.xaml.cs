33f7cf5dba22ab9bb92a59e1cd3755b94030e238
Sisi231 <silvi7andreeva@gmail.com>
2023-04-20 08:26:11 +0300

    Silvi project

--- Movies/LogIn.xaml.cs
+++ Movies/LogIn.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,7 +28,41 @@ namespace Movies
 
         private void Submit_Click(object sender, RoutedEventArgs e)
         {
+            SqlConnection sqlCon = new SqlConnection(@"Data Source=DESKTOP-9B7GMJ6\SQLEXPRESS; Initial Catalog=Movies; Integrated Security=True");
 
+            try
+            {
+                if (sqlCon.State == ConnectionState.Closed)
+                    sqlCon.Open();
+                string query = "SELECT COUNT(1) FROM SignUpTable Where Username=@Username and Password=@Password";
+                SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                sqlCmd.CommandType = CommandType.Text;
+                sqlCmd.Parameters.AddWithValue("@Username", txtUsername.Text);
+                sqlCmd.Parameters.AddWithValue("@Password", PasswordBox.Password);
+
+                int count = Convert.ToInt32(sqlCmd.ExecuteScalar());
+                if (count == 1)
+                {
+                    Menu OpenMenu = new Menu(txtUsername.Text);
+                    OpenMenu.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Username or password are not correct!");
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlCon.Close();
+            }
         }
 
         private void button_Click(object sender, RoutedEventArgs e)
---
+++ Movies/Menu.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Movies
+{
+    /// <summary>
+    /// Interaction logic for Menu.xaml
+    /// </summary>
+    public partial class Menu : Window
+    {
+        public string username;
+        public Menu(string AccountHolder)
+        {
+            username = AccountHolder;
+            InitializeComponent();
+        }
+
+        private void Submit_Click1(object sender, RoutedEventArgs e)
+        {
+            Account OpenAccount = new Account(username);
+            OpenAccount.Show();
+            this.Close();
+        }
+
+        private void Submit_Click2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Submit_Click3(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Submit_Click4(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
