b6028b4cde9fb1bfe9d188274f87cbfa2c64fffe
Sisi231 <silvi7andreeva@gmail.com>
2023-04-22 15:28:00 +0300

    111

--- Movies/Account.xaml.cs
+++ Movies/Account.xaml.cs
@@ -26,20 +26,21 @@ namespace Movies
         {
             InitializeComponent();
             username = AccountHolder;
+            fill_Data();
         }
         void fill_Data()
         {
-            SqlConnection sqlCon = new SqlConnection(@"Data Source=DESKTOP-9B7GMJ6\SQLEXPRESS; Initial Catalog=StudentRecords; Integrated Security=True");
+            SqlConnection sqlCon = new SqlConnection(@"Data Source=DESKTOP-9B7GMJ6\SQLEXPRESS; Initial Catalog=Movies; Integrated Security=True");
             try
             {
                 if (sqlCon.State == ConnectionState.Closed)
                     sqlCon.Open();
-                string query = $"SELECT * FROM SignUpTable Where Username = '{username}'";
+                string query = $"SELECT Username, Email FROM SignUpTable Where Username = '{username}'";
                 SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
                 SqlDataReader dr = sqlCmd.ExecuteReader();
                 dr.Read();
-                txtUsername.Text = dr.GetString(1);
-                txtEmail.Text = dr.GetString(2);
+                txtUsername.Text = dr.GetString(0);
+                txtEmail.Text = dr.GetString(1);
             }
             catch (Exception ex)
             {
@@ -65,8 +66,8 @@ namespace Movies
 
         private void Submit_Click1(object sender, RoutedEventArgs e)
         {
-            Menu OpenMenu = new Menu(username);
-            OpenMenu.Show();
+            ChangePassword OpenChangePassword = new ChangePassword(username);
+            OpenChangePassword.Show();
             this.Close();
         }
     }
---
+++ Movies/ChangePassword.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace Movies
+{
+    /// <summary>
+    /// Interaction logic for ChangePassword.xaml
+    /// </summary>
+    public partial class ChangePassword : Window
+    {
+        public string username;
+        public ChangePassword(string AccountHolder)
+        {
+            InitializeComponent();
+            username = AccountHolder;
+        }
+
+        private void Submit_Click(object sender, RoutedEventArgs e)
+        {
+            if (NewPasswordBox.Password.Length < 10 || NewPasswordBox.Password.Length > 10)
+            {
+                MessageBox.Show("Your password should be at least 10 symbols long! Try again");
+            }
+            else if (!(NewPasswordBox.Password == ReNewPasswordBox.Password))
+            {
+                MessageBox.Show("The passwords don't match!Try again.");
+            }
+            else
+            {
+                SqlConnection sqlCon = new SqlConnection(@"Data Source=DESKTOP-9B7GMJ6\SQLEXPRESS; Initial Catalog=Movies; Integrated Security=True");
+
+                try
+                {
+                    if (sqlCon.State == ConnectionState.Closed)
+                        sqlCon.Open();
+                    string query = "Update SignUpTable set Password = '"+NewPasswordBox.Password+"' Where Username='"+username+"'and Password ='"+ CurrentPassword.Password  + "'";
+                    SqlCommand cmd = new SqlCommand(query, sqlCon);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Password successfully changed!");
+                    Account OpenAccount = new Account(username);
+                    OpenAccount.Show();
+                    this.Close();
+                }
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    sqlCon.Close();
+                }
+            }
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Menu OpenMenu = new Menu(username);
+            OpenMenu.Show();
+            this.Close();
+        }
+    }
+}
